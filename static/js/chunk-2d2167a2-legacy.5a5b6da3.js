5fb9850580b7c7b1ba7f4bc8e454a0a5c3f224e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-10 07:40:23 +0000

    Deploy han-feng/cvicse-admin-start-kit to github.com/han-feng/cvicse-admin-start-kit.git:gh-pages

---
+++ static/js/chunk-2d2167a2-legacy.5a5b6da3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d2167a2"],{c340:function(t,e,n){"use strict";n.r(e);var a=n("2877"),s=Object(a.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("d2-container",{staticClass:"page"},[e("d2-page-cover",{attrs:{title:"I AM CVICSE ADMIN","sub-title":"企业内部应用前端模板"}})],1)},[],!1,null,null,null).exports;e.default=s}}]);
